66188b3e19dd6168b1c853f990c9701ef7cbdab9
shotaaaaaaaaa <sgac31tvgam@gmail.com>
2022-12-22 14:24:48 +0900

    blog post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,18 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     
     protected $fillable = [
         'title',
         'body'
         ];
-    
+        
     public function getpaginateByLimit(int $limit_count = 5)
     {
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this->orderby('updated_at', 'DESC')->paginate($limit_count);
     }
 }
